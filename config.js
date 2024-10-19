600ca2a37d12fa50cdab57406a365ece836ceea5
thatkids223 <147352169+thatkids223@users.noreply.github.com>
2024-10-19 15:59:59 +0000

    Update config.js

--- config.js
+++ config.js
@@ -16,7 +16,7 @@ const DEBUG = convertToBool(process.env.DEBUG, 'true');
 
 module.exports = {
   VERSION: 'v4.4.4',
-  SESSION_ID: process.env.SESSION_ID || '',
+  SESSION_ID: process.env.SESSION_ID || 'HP_2024_OCT_19_5QKOC5',
   MODE: (process.env.MODE || 'private').toLowerCase(),
   HANDLERS: (process.env.PREFIX || '^[.,!]').trim(),
   SEND_READ: toBool(process.env.READ_COMMAND),
